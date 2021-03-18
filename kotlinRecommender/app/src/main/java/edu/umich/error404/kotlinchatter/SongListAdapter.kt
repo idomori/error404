2a817f0da3f4ad8bc2da961f1779f258f00b7aeb
William DePender <77747482+widepend@users.noreply.github.com>
2021-03-17 21:25:32 -0400

    Front end and back end communicating

---
+++ kotlinRecommender/app/src/main/java/edu/umich/error404/kotlinchatter/SongListAdapter.kt
@@ -0,0 +1,29 @@
+package edu.umich.error404.kotlinchatter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import edu.umich.error404.kotlinchatter.databinding.ListitemChattBinding
+
+class SongListAdapter(context: Context, users: ArrayList<Song?>) :
+    ArrayAdapter<Song?>(context, 0, users) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val item = getItem(position)
+
+        val listItemViewById = ListitemChattBinding.bind(convertView ?:
+        LayoutInflater.from(context).inflate(R.layout.listitem_chatt, parent, false))
+
+
+        item?.run {
+            listItemViewById.songTextView.text = "Song = " + song
+            listItemViewById.artistTextView.text = "Artist = " + artist
+            listItemViewById.keyTextView.text = "Key = " + key
+            listItemViewById.tempoTextView.text = "Tempo = " + tempo
+            listItemViewById.danceabilityTextView.text = "Danceability = " + danceability
+        }
+        return listItemViewById.root
+    }
+}
